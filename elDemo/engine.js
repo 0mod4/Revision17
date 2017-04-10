ee22d6b14c4b915ab4df953cc2cbe80db3b73407
nvalis <no.va.lis@gmx.net>
2017-04-10 13:55:39 +0200

    Add stats.js

--- elDemo/engine.js
+++ elDemo/engine.js
@@ -5,6 +5,7 @@ var State = { Test: 0 };
 engine.state = State.Test;
 engine.isReady = false;
 
+var stats;
 var testScene;
 var gl, m4 = twgl.m4;
 var planetVideo;
@@ -15,29 +16,33 @@ var bufferInfo;
 //var music;
 //var musicIsReady = false;
 
-	var vert =         
-        "attribute vec4 position;"+
-		"void main() {"+
-		"	gl_Position = position;"+
-		"}";
+var vert =
+	"attribute vec4 position;"+
+	"void main() {"+
+	"	gl_Position = position;"+
+	"}";
 
-	var frag = //caution! have a problem with comments in this format
-		"precision mediump float;"+
-		"uniform vec2 resolution;"+
-		"uniform float time;"+
-		"void main() {"+
-		"	vec2 uv = gl_FragCoord.xy / resolution;"+
-		"	float color = 0.0;"+
-		"	color += sin( uv.x * cos( time / 3.0 ) * 60.0 ) + cos( uv.y * cos( time / 2.80 ) * 10.0 );"+
-		"	color += sin( uv.y * sin( time / 2.0 ) * 40.0 ) + cos( uv.x * sin( time / 1.70 ) * 40.0 );"+
-		"	color += sin( uv.x * sin( time / 1.0 ) * 10.0 ) + sin( uv.y * sin( time / 3.50 ) * 80.0 );"+
-		"	color *= sin( time / 10.0 ) * 0.5;"+
-		"	gl_FragColor = vec4( vec3( color * 0.5, sin( color + time / 2.5 ) * 0.75, color ), 1.0 );"+
-		"}";
+var frag = //caution! have a problem with comments in this format
+	"precision mediump float;"+
+	"uniform vec2 resolution;"+
+	"uniform float time;"+
+	"void main() {"+
+	"	vec2 uv = gl_FragCoord.xy / resolution;"+
+	"	float color = 0.0;"+
+	"	color += sin( uv.x * cos( time / 3.0 ) * 60.0 ) + cos( uv.y * cos( time / 2.80 ) * 10.0 );"+
+	"	color += sin( uv.y * sin( time / 2.0 ) * 40.0 ) + cos( uv.x * sin( time / 1.70 ) * 40.0 );"+
+	"	color += sin( uv.x * sin( time / 1.0 ) * 10.0 ) + sin( uv.y * sin( time / 3.50 ) * 80.0 );"+
+	"	color *= sin( time / 10.0 ) * 0.5;"+
+	"	gl_FragColor = vec4( vec3( color * 0.5, sin( color + time / 2.5 ) * 0.75, color ), 1.0 );"+
+	"}";
 
 
 engine.init = function ()
 {
+		stats = new Stats();
+		stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+		document.body.appendChild(stats.dom);
+
 		gl = twgl.getWebGLContext(document.getElementById("c"), { premultipliedAlpha: false, alpha: false });
 		gl.getExtension("OES_texture_float");
 		gl.getExtension("OES_texture_float_linear");
@@ -68,6 +73,7 @@ engine.init = function ()
 };
 
 function render(time) {
+	stats.begin();
 	twgl.resizeCanvasToDisplaySize(gl.canvas);
 	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 	// var uniforms = {
@@ -81,6 +87,8 @@ function render(time) {
 	testScene.update();
 	if (gl.getError() !== 0)
 		console.log(gl.getError());
+	stats.end();
+
 	requestAnimationFrame(render);
 }
 // engine.init = function ()
